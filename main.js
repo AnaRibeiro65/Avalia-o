7882b6c6865de8f1c0fa324395bd12654ec92d4f
AnaRibeiro65 <106127641+AnaRibeiro65@users.noreply.github.com>
2023-03-10 22:26:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const tex = document.querySelector('.primeTexto');
+
+//  function fun(){
+//      tex.innerHTML = num;
+//  }
+
+ function numero(num){
+     tex.innerHTML += `Você selecionou  ${num} de 5`
+ }
+
+function concluido(){
+    let pri = document.getElementById('pri').style.display = 'block'
+ }
+
